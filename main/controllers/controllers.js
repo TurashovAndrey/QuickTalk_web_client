fb0ef958f3304e22f68a0de94faea48a0018bdd9
Andrey Turashov <turashov@mail.ru>
2014-05-19 20:49:57 +0300

    First attempt to get data from the server

--- main/controllers/controllers.js
+++ main/controllers/controllers.js
@@ -1,10 +1,9 @@
-function PhoneListCtrl($scope) {
-  $scope.phones = [
-    {"name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S."},
-    {"name": "Motorola XOOM with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet."},
-    {"name": "MOTOROLA XOOM",
-     "snippet": "The Next, Next Generation tablet."}
-  ];
-}
+var phonecatApp = angular.module('phonecatApp', []);
+ 
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  $http.get('http://fahlo.loc/leasing/getAdvert').success(function(data) {
+    $scope.phones = data;
+  });
+ 
+  $scope.orderProp = 'age';
+});
