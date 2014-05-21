3b7066615a0d25956977ea361c2ce6b0beb1e5ec
Andrey Turashov <turashov@mail.ru>
2014-05-21 21:06:15 +0300

    Small adjustment in getting data

--- main/controllers/controllers.js
+++ main/controllers/controllers.js
@@ -2,7 +2,7 @@ var phonecatApp = angular.module('phonecatApp', []);
  
 phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
   $http.get('http://fahlo.loc/leasing/getAdvert').success(function(data) {
-    $scope.phones = data;
+    $scope.advert = data;
   });
  
   $scope.orderProp = 'age';
