3ab4e97997bbdb3a703bfa3b4b3189fb82c0ee3d
Andrey Turashov <turashov@mail.ru>
2014-06-24 09:45:08 +0300

    Improve get cities functionality

--- js/controllers/MyCtrl.js
+++ js/controllers/MyCtrl.js
@@ -25,19 +25,11 @@ angular.module("leasingApp").factory('MovieRetriever', function($http, $q, $time
   return MovieRetriever;
 });
 
-angular.module("leasingApp").controller('MyCtrl', function($scope, MovieRetriever){
-        $scope.movies = ["Lord of the Rings",
-                        "Drive",
-                        "Science of Sleep",
-                        "Back to the Future",
-                        "Oldboy"];
-
-        // gives another movie array on change
-        $scope.updateMovies = function(typed){
-            // MovieRetriever could be some service returning a promise
-            $scope.newmovies = MovieRetriever.getmovies(typed);
-            $scope.newmovies.then(function(data){
-              $scope.movies = data;
-            });
-        }
+angular.module("leasingApp").controller('MyCtrl', function($scope, MovieRetriever, CitiesService){
+                        $scope.movies = Array()
+        		CitiesService.get_cities().then(function(data) {
+			    for (index = 0; index < data.cities.length; ++index) {
+                               $scope.movies.push(data.cities[index].city_name);
+			    }
+                        });
     });
